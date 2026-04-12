1f84fe9145d34582d7641deb1f6b32331b4c0b5d
Yannick Schawaller <yannick.schawaller@gmx.de>
2026-04-12 12:58:32 +0200

    gemini model fix

--- script.js
+++ script.js
@@ -285,7 +285,7 @@ async function frageGeminiAn() {
     `;
 
     try {
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${key}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${key}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
